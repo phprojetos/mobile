a9c6059e79aea04227bb36aa9a6469b0d674d4ef
phprojetos <paulo.h.soares@iesb.edu.br>
2025-03-18 21:56:04 -0300

    atitividade5

--- exercicios/05- expo go/04-indrucao-react-native/App.js
+++ exercicios/05- expo go/04-indrucao-react-native/App.js
@@ -1,22 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const nome = "Paulo"
+  const nome = "thirrasgo"
   function alerta(){
     alert("clicou no botao")
   }
   return (
+    <ScrollView>
     <View style={styles.container}>
       <StatusBar style='auto'/>
-      <Text>Meu projeto {nome}</Text>
+      <Text>Meu projeto teste {nome}</Text>
       <Image
       source={{
         uri:"https://s2-oglobo.glbimg.com/cdz6qJ-uQ1_A3xsoJbYBXluXy3c=/0x0:1324x883/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2025/P/V/W9YNsMS1GsZlXFZOOUMQ/110344267-fluminense-x-flamengo-campeonato-carioca-maracana-12-03-2025-82.-fotos-gilvan-de-sou-2-.jpg"
       }}
       style={{
-        height:400,
-        width:400
+        height:200,
+        width:200
+      }}
+      />
+      <Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      />
+      <Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      /><Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      /><Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      /><Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      />
+      <Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
+      }}
+      />
+      <Image
+      source={ require('./imagens/image.png')
+      }
+      style={{
+        height:200,
+        width:200
       }}
       />
 
@@ -26,13 +80,14 @@ export default function App() {
       <Text>Dificil</Text>
       <Button title='Clicar' onPress={alerta}></Button>
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
