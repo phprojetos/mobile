b6822d76136e25ce7980202411b7ac861876e751
phprojetos <paulo.h.soares@iesb.edu.br>
2025-04-04 23:47:17 -0300

    atualização

---
+++ exercicios/05- expo go/07-exercicio/lista-times-jogadores/components/Jogador.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Avatar, Card } from 'react-native-paper';
+
+export default function Jogador(props) {
+
+  const { nome,numero,imagem } = props
+
+  return (
+    <Card.Title
+      title={nome}
+      subtitle={`Numero ${numero}`}
+      left={(props) => <Avatar.Image {...props} source={{ uri: imagem }} />}
+    />
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ exercicios/05- expo go/07-exercicio/lista-times-jogadores/components/Time.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FlatList, StyleSheet } from 'react-native'
+import { Card, Text } from 'react-native-paper'
+import {Jogador} from './Jogador'
+
+export default function Time(props) {
+
+  const { nome, anoFundacao, mascote, imagem,jogadores } = props
+
+  return (
+    <Card style={{ margin: 10 }}>
+      <Card.Title title={nome} subtitle={anoFundacao} />
+      <Card.Content>
+        <Text>{mascote}</Text>
+      </Card.Content>
+      <Card.Cover source={{ uri: imagem }} />
+      <Card.Actions>
+        <FlatList
+          horizontal
+          data={jogadores}
+          renderItem={({ item }) => (
+            <Jogador
+              nome={item.nome}
+              numero={item.numero}
+              imagem={item.imagem}
+            />
+          )}
+        />
+      </Card.Actions>
+    </Card>
+  )
+}
+
+const styles = StyleSheet.create({})
